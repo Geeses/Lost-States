f73c6e5240adefbcd929bc56d382b4ab7aff04cd
Oliver Wobst <o.wobst@web.de>
2020-05-29 02:16:32 +0200

    Added GridManager
    -GridManager can read TilePrefabs and place them into an array
    -added testmap to test GridManager functionality
    -changed Grid-Cellsize back to 1 (from 0.64), reduced Sprites Pixels per Unit to 64 (from 100) -> same effect, less pain in the future

---
+++ Assets/Scripts/GridManager.cs
@@ -0,0 +1,97 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridManager : MonoBehaviour
+{
+    [SerializeField]
+    private List<GameObject> children;
+    public GameObject [,] tilearray;
+
+    void Awake()
+    {
+        // initialize min and max values for x and z dimensions, setting them by comparing to transforms of children
+        float xmin = Mathf.Infinity;
+        float xmax = Mathf.NegativeInfinity;
+        float zmin = Mathf.Infinity;
+        float zmax = Mathf.NegativeInfinity;
+
+        foreach (Transform child in transform)
+        {
+            children.Add(child.gameObject);
+
+            if (child.transform.position.x < xmin)
+            {
+                xmin = child.transform.position.x;
+            }
+            else if (child.transform.position.x > xmax)
+            {
+                xmax = child.transform.position.x;
+            }
+
+            if (child.transform.position.z < zmin)
+            {
+                zmin = child.transform.position.z;
+            }
+            else if (child.transform.position.z > zmax)
+            {
+                zmax = child.transform.position.z;
+            }
+
+            print(child + " at " + "x:" + child.transform.position.x.ToString() + "z:" + child.transform.position.z.ToString());  // debug   
+        }
+
+        // calculate rows in x and z dimension
+        float xrows = Mathf.Abs(xmin - xmax) + 1;
+        float zrows = Mathf.Abs(zmin - zmax) + 1;
+
+        // check if row-vars are decimals, if so an exception is thrown as no valid array can be created
+        if(xrows % 1 != 0 || zrows % 1 != 0)
+        {
+            throw new System.Exception("Cannot create a valid Array! (Distances between tiles cannot be decimal)");
+        }
+
+        tilearray = new GameObject[(int)xrows, (int)zrows];
+
+        /* put every element from the GameObject List from above in the right spot of the array by taking
+           the top left most tile of all tiles (the one that would be if its not a rectangle) as starting
+           point and iterate trough the tile-area (boundaries were determined above) placing a tile at
+           array[x,z] if its transform matches the searched one (calculated by checkvector) for the specific 
+           field. also has a check for cells that have multiple tiles in them */ 
+        for (int z = 0; z <= (int)zrows; z++)
+        {
+            for (int x = 0; x <= (int)xrows; x++) 
+            {
+                Vector3 checkvector = new Vector3(xmin + x, 0f, zmax - z);
+
+                foreach (GameObject child in children)
+                {
+                    if(child.transform.position == checkvector)
+                    {
+                        if (tilearray[x,z] != null)
+                        {  
+                            throw new System.Exception(string.Format("Position {0} in the grid has multiple assigned tiles! (It may only have one per cell)", checkvector));
+                        }
+                        tilearray[x,z] = child;
+                    } 
+                }
+            }          
+        }
+
+        // debug-prints to check if all elements are on the correct array-positions
+        for (int i = 0; i < zrows; i++)
+        {
+            for (int j = 0; j < xrows; j++)
+            {
+                if(tilearray[j,i] == null)
+                {
+                    print(j.ToString() + "||" + i.ToString() + " " + "null");
+                }
+                else
+                {
+                    print(j.ToString() + "||" + i.ToString() + " " + tilearray[j,i].transform.position.ToString());
+                }
+            }
+        }
+    }
+}
